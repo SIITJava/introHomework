784d0a2d47c28f574212d41824af378b95e7c45e
FloreaRobertMarian <164220631+RobertFM980@users.noreply.github.com>
2024-03-25 19:25:46 +0200

    First homework

--- src/main/java/ro/siit/AlgorithmCaller.java
+++ src/main/java/ro/siit/AlgorithmCaller.java
@@ -7,32 +7,52 @@ public class AlgorithmCaller {
 
     // 1. Calculate the sum of the first 100 numbers higher than 0
     public int sumOfFirst100Numbers() {
-        // Your implementation here
-        return 0; // Placeholder return value
+        int s;
+        s=0;
+        for (int i=1;i<=100;i++)
+            s=s+i;
+        return s;
     }
 
     // 2. Display the smallest number from an array of numbers
     public int findSmallestNumber(int[] numbers) {
-        // Your implementation here
-        return 0; // Placeholder return value
+        int min=numbers[0];
+        for (int i=1;i<numbers.length;i++)
+            if(min>numbers[i])min=numbers[i];
+        return min;
     }
 
     // 3. Display the max digit of a number
     public int findMaxDigit(int number) {
-        // Your implementation here
-        return 0; // Placeholder return value
+        int i=number%10;
+        while(number!=0){
+            if(number%10>i)i=number%10;
+            number=number/10;
+        }
+        return i;
     }
 
     // 4. Check if a number is palindrome
     public boolean isPalindrome(int number) {
-        // Your implementation here
-        return false; // Placeholder return value
+        int b1=0,aux=number;
+        while(aux>0){
+            b1=b1*10+aux%10;
+            aux=aux/10;
+        }
+        return number==b1;
+
     }
 
     // 5. Display all the prime numbers lower than a given number
     public List<Integer> findPrimes(int limit) {
-        // Your implementation here
-        return new ArrayList<>(); // Placeholder return value
+        int nr=0;
+        ArrayList<Integer> primes = new ArrayList<>();
+        for(int i=2;i<=limit;i++) {
+            for (int d = 2; d <= i / 2; d++) if (i % d == 0) nr++;
+            if (nr == 0) primes.add(i);
+            else nr = 0;
+    }
+        return primes;
     }
 }
 
